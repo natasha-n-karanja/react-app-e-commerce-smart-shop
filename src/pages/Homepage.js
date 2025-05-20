5bfa6b78f6332c02ff8780b7eccb7b5e612faf5a
natashankaranja <natashankaranja@gmail.com>
2025-05-20 17:37:33 +0300

    added homepage

---
+++ src/pages/Homepage.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+import { Link } from 'react-router-dom';
+
+function Homepage () {
+  return(
+    <h1>Welcome to my Smart Shop!</h1>
+  )
+}
+
+export default Homepage;
